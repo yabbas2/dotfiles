92fdb9d08e4f2e7860c04a558590d4df66900fee
yabbas2 <yousef.abbas.2@outlook.com>
2024-11-06 17:17:41 +0100

    Update colorscheme and telescope highlights

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -4,13 +4,15 @@ return {
     config = function()
         require('catppuccin').setup({
             flavour = "mocha",
-            transparent_background = true,
+            -- transparent_background = true,
             default_integrations = false,
             integrations = {
-                telescope = true,
+                telescope = {
+                    enabled = true,
+                    style = "nvchad"
+                },
                 mason = true,
                 notify = true,
-                render_markdown = true,
                 cmp = true,
                 dap = true,
                 dap_ui = true,
@@ -21,6 +23,32 @@ return {
                 ufo = true,
                 overseer = true,
                 gitsigns = true,
+                dropbar = {
+                    enabled = true,
+                    color_mode = true,
+                },
+                indent_blankline = true,
+                noice = true,
+                native_lsp = {
+                    enabled = true,
+                    virtual_text = {
+                        errors = { "italic" },
+                        hints = { "italic" },
+                        warnings = { "italic" },
+                        information = { "italic" },
+                        ok = { "italic" },
+                    },
+                    underlines = {
+                        errors = { "undercurl" },
+                        hints = { "undercurl" },
+                        warnings = { "undercurl" },
+                        information = { "undercurl" },
+                        ok = { "undercurl" },
+                    },
+                    inlay_hints = {
+                        background = true,
+                    },
+                },
             },
             term_colors = true,
         })
